92044a4c73680a5f8446355730c3c6b9d185cd4c
Newlearn1234 <123717361+Newlearn1234@users.noreply.github.com>
2023-10-31 15:53:59 +0530

    Update program.cs

--- program.cs
+++ program.cs
@@ -1,43 +1,9 @@
-// using System;
-// partial class Program
-// {
-//     static void Main()
-//     {
-//         Console.WriteLine("Hello, World!");
-//     }
-// }
-
 using System;
-using System.Xml;
-
-class Program
+partial class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello, World!"); // Add this line to print "Hello, World!"
-
-        // Specify the path to your XML file
-        string xmlFilePath = "extra.xml"; // Replace with your XML file's path
-
-        try
-        {
-            // Load the XML document
-            XmlDocument xmlDoc = new XmlDocument();
-            xmlDoc.Load(xmlFilePath);
-
-            // Access XML elements and attributes
-            XmlNodeList nodes = xmlDoc.SelectNodes("/root/element");
-
-            foreach (XmlNode node in nodes)
-            {
-                string value = node.InnerText;
-                Console.WriteLine(value);
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Error: " + ex.Message);
-        }
+        Console.WriteLine("Hello, World!");
     }
 }
 
